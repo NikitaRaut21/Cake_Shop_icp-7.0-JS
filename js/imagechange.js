aac0792f6e8cbe087b36054f7947087ff6276a41
NikitaRaut21 <rautn7350@gmail.com>
2024-05-10 12:55:29 +0530

    updated js

---
+++ js/imagechange.js
@@ -0,0 +1,21 @@
+function ChangeImage(num){
+    const MainImage = document.getElementById('Main-Product');
+    if(num==1){
+        MainImage.src = "./../images/real img1.jpeg";
+    }
+    else if(num==2){
+        MainImage.src = "./../images/real img 2.jpeg";
+    }
+    else if(num==3){
+        MainImage.src = "./../images/real img 3.jpeg";
+    }
+    else if(num==4){
+        MainImage.src = "./../images/real img 4.jfif";
+    }
+    else if(num==5){
+        MainImage.src = "./../images/real img 5.jfif";
+    }
+  
+    
+
+}
